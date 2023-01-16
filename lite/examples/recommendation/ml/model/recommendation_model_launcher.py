1ce064daf0aef8ee8c79d3f5c266fc7be792f9c4
NirupamaAbraham <36266779+NirupamaAbraham@users.noreply.github.com>
2023-01-16 17:44:59 +0530

    Added logging

--- lite/examples/recommendation/ml/model/recommendation_model_launcher.py
+++ lite/examples/recommendation/ml/model/recommendation_model_launcher.py
@@ -266,6 +266,10 @@ def prepare_model_config():
 
 def main(_):
   logger = tf.get_logger()
+  
+  tf.print("model_dir:", FLAGS.model_dir)
+  tf.print("export_dir:", FLAGS.export_dir)
+  
   if not tf.io.gfile.exists(FLAGS.model_dir):
     tf.io.gfile.mkdir(FLAGS.model_dir)
 
